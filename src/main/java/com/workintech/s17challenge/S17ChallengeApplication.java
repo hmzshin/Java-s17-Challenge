3e54d0435d4979e5dcebf026ef4221fda57f90fb
hmzshin <hamzasahin@outlook.com>
2024-02-11 18:54:42 +0300

    initial commit

---
+++ src/main/java/com/workintech/s17challenge/S17ChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.workintech.s17challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S17ChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S17ChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workintech/s17challenge/S17ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.s17challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S17ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
